9f6b15c38d2d3d6c57989c97fb453770b3382080
Artem Liskovshchenko <artem140742@gmail.com>
2024-01-11 17:58:00 +0200

    first push

---
+++ src/js/1-gallery.js
@@ -0,0 +1,28 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import images from './gallery-images';
+
+const gallery = document.querySelector('.gallery');
+
+const markup = images
+    .map(({ preview, original, description }) => {
+        return `
+        <li class="gallery-item">
+            <a class="gallery-link" href="${original}">
+                <img class="gallery-image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+    `
+    })
+    .join('');
+
+gallery.insertAdjacentHTML('afterbegin', markup);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+
+});
+
+lightbox.on('show.simplelightbox', function () {
+
+});
